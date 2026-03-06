2678887da3f1ec5971ce671f69a2523b1d2f0911
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-03-06 10:44:25 -0800

    llama: update error message

--- llama/error_types.go
+++ llama/error_types.go
@@ -4,7 +4,7 @@ type ErrEmptyCommand struct{}
 
 // Error implements error.
 func (e ErrEmptyCommand) Error() string {
-	return "ramalama command should not be empty"
+	return "llama command should not be empty"
 }
 
 var _ error = ErrEmptyCommand{}
